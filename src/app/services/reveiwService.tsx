94d8ac42ec8dd99cab6944f1428bc870b635847e
rohitkumarisearch-developerfullstack <rohitkumar.isearch@gmail.com>
2026-04-13 15:13:57 +0530

    update all changs and convert all img into webp and correct content

--- src/app/services/reveiwService.tsx
+++ src/app/services/reveiwService.tsx
@@ -2,28 +2,28 @@ const baseUrl = process.env.NEXT_PUBLIC_UAT_URL;
 const XPublicToken =
   "zaxsc+/-=0dfvgbnhmjklo*/-piutyerwq*%$25631478907539541lokythbfet&*(@kjhkhgfhk546456456)";
 
-  
-  export async function fetchPackageReviewData() {
 
-       const res = await fetch(`${baseUrl}/api/v1/reviews/packages`, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            "X-Public-Token": XPublicToken,
-          },
-       next: { revalidate: 60 }
-    });
-  
-    if (!res.ok) {
-      throw new Error("Failed to fetch data");
-    }
-  
-    return res.json();
+export async function fetchPackageReviewData() {
+
+  const res = await fetch(`${baseUrl}/api/v1/reviews/packages`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Public-Token": XPublicToken,
+    },
+    next: { revalidate: 60 }
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
   }
-  
+
+  return res.json();
+}
 
 
-  export async function fetchCarReviewData() {
+
+export async function fetchCarReviewData() {
   const res = await fetch(
     `${baseUrl}/api/v1/realtime/reviews/cars`,
     {
